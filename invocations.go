adce0f3a3ede3f7e538134916f725fba1878e491
Yann D'Isanto <ydisanto@gmail.com>
2024-10-30 14:38:10 +0100

    feat: invocation request with url encoded form

--- invocations.go
+++ invocations.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"net/url"
 
 	assertions "github.com/stretchr/testify/assert"
 )
@@ -13,6 +14,11 @@ type Invocation struct {
 	request   *http.Request
 	payload   []byte
 	testState TestingT
+}
+
+type InvocationRequestForm struct {
+	invocation *Invocation
+	formValues url.Values
 }
 
 func newInvocation(request *http.Request, testState TestingT) *Invocation {
@@ -69,5 +75,30 @@ func (call *Invocation) ReadJSONPayload(obj any) {
 	err := json.Unmarshal(call.GetPayload(), obj)
 	if err != nil {
 		call.testState.Fatal(err)
+	}
+}
+
+func (call *Invocation) WithUrlEncodedFormPayload() *InvocationRequestForm {
+	formValues, err := url.ParseQuery(string(call.GetPayload()))
+	if err != nil {
+		call.testState.Fatal(err)
+		return nil
+	}
+	return &InvocationRequestForm{
+		invocation: call.WithHeader("Content-Type", "application/x-www-form-urlencoded"),
+		formValues: formValues,
+	}
+}
+
+func (form InvocationRequestForm) WithValues(expectedValues map[string]string) {
+	for key, value := range expectedValues {
+		assertions.Equal(form.invocation.testState, value, form.formValues.Get(key))
+	}
+}
+
+func (form InvocationRequestForm) WithValuesExactly(expectedValues map[string]string) {
+	assertions.Equal(form.invocation.testState, len(expectedValues), len(form.formValues))
+	for key, value := range expectedValues {
+		assertions.Equal(form.invocation.testState, value, form.formValues.Get(key))
 	}
 }
